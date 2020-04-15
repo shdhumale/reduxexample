d491475c7046e7d12131c026a07240c4718833dc
Siddharatha.H.Dhumale <shdhumale@gmail.com>
2020-04-15 21:14:40 +0530

    Redux simple example

--- multiplereducer.js
+++ multiplereducer.js
@@ -1,6 +1,12 @@
 //console.log("this is test")
 const redux = require("redux")
+const reduxLogger = require("redux-logger")
 const createStore = redux.createStore
+const logger = reduxLogger.createLogger({
+    //empty options
+});
+const applyMiddleWare = redux.applyMiddleware
+
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -21,15 +27,19 @@ function buyIceCream() {
 
 //reducer funtions (prevState, action) ==> return newState
 
-const initialState = {
-    numberOfCake: 10,
+const initialCakeState = {
+    numberOfCake: 10
+}
+
+const initialIceCreamState = {
     numberOfIceCream: 20
 }
 
 
+
 //'- Reducer:- Actully carrier out the change in the state. i.e. we need to write the code in reducer (pure function that teake prevState and action as input) and change the new state depending. so how the state is transfer is depending on this reducer.
 //In short our application subscribe to redux, it store its state in redux, It cannot change the state directly it has to emit / dispath the action which is given to reducer which is pure function depending on the action type it will modify the state and then redux inform / send the new state to our application as it is subscribed.
-const reducer = (state = initialState, action) => {
+const reducerCake = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
             ...state,
@@ -37,6 +47,15 @@ const reducer = (state = initialState, action) => {
 
         }
 
+        default:
+            return state
+
+    }
+
+}
+
+const reducerIceCream = (state = initialIceCreamState, action) => {
+    switch (action.type) {
         case BUY_ICECREAM: return {
             ...state,
             numberOfIceCream: state.numberOfIceCream - 1
@@ -52,9 +71,12 @@ const reducer = (state = initialState, action) => {
 //		'- Store :- GSD - Getstate - to get the state of the application, susbcribe(listener) for subscrbing and unsubscribing  the listener as the state change and dispatch(action) is used to make change in state in the store.
 //It has four part 1- Hold application state, get the state, subscribe the application, dispacth action and finally unsubscribe
 //const store = createStore(reducer)
-const store = redux.createStore(reducer)
+const multipleReducer = redux.combineReducers({ cake: reducerCake, iceCream: reducerIceCream })
+//const store = redux.createStore(reducer)
+const store = redux.createStore(multipleReducer, applyMiddleWare(logger))
 console.log("Initial state", store.getState())
-const unsubscribe = store.subscribe(() => { console.log('Updated state', store.getState()) })
+//const unsubscribe = store.subscribe(() => { console.log('Updated state', store.getState()) })
+const unsubscribe = store.subscribe(() => { })
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
